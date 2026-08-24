18989cb04399fe01d598ad3d1babae0e00947f5f
Kalana <kalanajinendra@gmail.com>
2026-08-25 00:55:21 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Kalana";
+        int age = 22;
+        double gpa = 3.59;
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Is student: " + isStudent);
+    }
+}
