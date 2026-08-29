07f0c5e5f3b83e6ca5f506c01d485841e1ddd2fe
Kalana <kalanajinendra@gmail.com>
2026-08-29 21:07:12 +0530

    Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
 
+    public static void main(String[] args) {
+        Student student = new Student(
+                "2023/CS/082",
+                "Kalana",
+                "Computer Science"
+        );
 
+        Instructor instructor = new Instructor(
+                "INS001",
+                "Mr. Welgama",
+                "Computer Science"
+        );
+
+        System.out.println(student.getSummary());
+        System.out.println(instructor.getSummary());
+    }
 }
